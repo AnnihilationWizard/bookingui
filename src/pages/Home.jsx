f7b322f1e8b69a82865fd5bbec182543b5050546
AnnihilationWizard <Annihilation.Wizard@gmail.com>
2024-09-03 17:17:59 +0800

    Add Home page and related components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home
