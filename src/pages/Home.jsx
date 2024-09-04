b81d14e4d8ff6e28be5a71cb7ad0c5863c100e2c
AnnihilationWizard <Annihilation.Wizard@gmail.com>
2024-09-04 16:54:59 +0800

    Add fontawesome dependencies and update header and navbar styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Header from '../component/Header'
 import './home.scss'
 
 const Home = () => {
     return (
         <div className='home'>
             <Navbar />
+            <Header />
         </div>
     )
 }
