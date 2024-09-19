09a2dbafd441356212e9879a793d674b4b0aae47
AnnihilationWizard <Annihilation.Wizard@gmail.com>
2024-09-19 16:58:38 +0800

    Refactor component file structure and styles, and add new feature for browsing accommodation types and cities

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Categories from '../subcomponents/Categories'
+import { CategoriesCities, CategoriesType } from '../data'
 
 const Feature = () => {
     return (
-        <div>Feature</div>
+        <div className='feature'>
+            <div className='listTitle'>
+                <h2>依住宿類型瀏覽</h2>
+            </div>
+            <div className='listItems'>
+                <Categories dataArray={CategoriesType} />
+            </div>
+            <div className='listItems'>
+                <Categories dataArray={CategoriesCities} />
+            </div>
+        </div>
     )
 }
 
