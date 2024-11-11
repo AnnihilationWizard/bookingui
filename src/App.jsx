8f2edc6ba9a9460a7e187e0acba5d871bc67a111
AnnihilationWizard <Annihilation.Wizard@gmail.com>
2024-11-11 17:16:17 +0800

    新增 react-router-dom 支援，實作酒店列表頁面及搜尋功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Home from "./pages/Home";
 import "./app.scss";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HotelList from './pages/HotelList'
+import Hotel from "./pages/Hotel";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotelsList" element={<HotelList />} />
+          <Route path="/hotels/:Id" element={<Hotel />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/HotelList.jsx
+++ src/pages/HotelList.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SearchItem from '../components/SearchItem'
+import Navbar from '../components/Navbar'
+import "./hotelsList.scss"
+import { DateRange } from 'react-date-range'
+import { format } from 'date-fns'
+import * as locales from 'react-date-range/dist/locale';
+import { useLocation } from 'react-router-dom'
 
 const HotelList = () => {
+    const [openCalendar, setOpenCalendar] = useState(false);
+    const [destination, setDestination] = useState("");
+
+    const [dates, setDates] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection',
+        }
+    ]);
+
+    const [conditions, setConditions] = useState(
+        {
+            adult: 1,
+            children: 0,
+            room: 1
+        }
+    );
+
     return (
-        <div>HotelList</div>
+        <>
+            <div>
+                <Navbar />
+                <div className='listContainer'>
+                    <div className='listWrapper'>
+                        <div className='listSearch'>
+                            <div className='searchTitle'>
+                                搜尋
+                            </div>
+                            <div className='listItem'>
+                                <label>目的地 / 住宿名稱 : </label>
+                                <input type="text" className="searchInput" placeholder={destination === "" ? '要去哪裡?' : destination} onChange={(e) => setDestination(e.target.value)} />
+                            </div>
+                            <div className='listItem'>
+                                <label>入住 / 退房日期 {format(dates[0].startDate, "MM/dd/yyyy")} - {format(dates[0].endDate, "MM/dd/yyyy")}</label>
+                                <span className='dates'>
+                                    <div className="searchInput" onClick={() => setOpenCalendar(!openCalendar)} >入住時間 - 退房時間</div>
+                                    {openCalendar && <DateRange
+                                        editableDateInputs={true}
+                                        onChange={item => setDates([item.selection])}
+                                        moveRangeOnFirstSelection={false}
+                                        className="calendar"
+                                        ranges={dates}
+                                        minDate={new Date()}
+                                    //locale={locales['zhTW']}
+                                    />}
+                                </span>
+                            </div>
+                            <div className='listItem'>
+                                <div className='listItemLimitPrice'>
+                                    <span className='limitTitle'>每晚最低價格</span>
+                                    <input type="text" className='searchInput' />
+                                </div>
+                                <div className='listItemLimitPrice'>
+                                    <span className='limitTitle'>每晚最高價格</span>
+                                    <input type="text" className='searchInput' />
+                                </div>
+                                <div className='listItmConditions'>
+                                    <span className='SearchText'>3位成人，2位小孩，1間房間</span>
+                                </div>
+                            </div>
+                            <div className='listItem'>
+                                <button className='searchbtn'>搜尋</button>
+                            </div>
+                        </div>
+                        <div className='listResult'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
